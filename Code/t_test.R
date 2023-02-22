7937e82e6355fc3f4405bba1e0d36f5ee1a41fed
David Stone <stoned4@byu.edu>
2023-02-22 13:15:02 -0700

    Changed to two sided t test

--- Code/t_test.R
+++ Code/t_test.R
@@ -8,10 +8,26 @@ out = c("Gene", "P-value", "Fold_Change")
 for (i in 3:nrow(data)) {
 	tumor = as.list(as.data.frame(t(data[i, which(data[2,] == "Tumor")])))
 	normal = as.list(as.data.frame(t(data[i, which(data[2,] == "Normal")])))
-	result = t.test(as.numeric(normal[[1]]), as.numeric(tumor[[1]]), alternative = c('l'))
-	foldCchange = log2(result$estimate[[2]]/result$estimate[[1]])
-	out = rbind(out, c(data[i,1], result$p.value, foldCchange))	
+	tumor = unlist(lapply(tumor[[1]], function(x){x[x != ""]}))
+	normal = unlist(lapply(normal[[1]], function(x){x[x != ""]}))
+	if (length(tumor) > 1 && length(normal) > 1) {
+		result = t.test(as.numeric(normal), as.numeric(tumor))
+		foldChange = log2(result$estimate[[2]]/result$estimate[[1]])
+		out = rbind(out, c(data[i,1], result$p.value, foldChange))
+	}
+	
+	# if (foldChange != Inf) {
+		
+	# }
 }
+if (length(out) > 3) {
+	names = out[1,]
+	out = data.table(out[-c(1),])
+	setnames(out, names)
+} else {
+	out = list("Not enough data")
+}
+
 
 fwrite(out, snakemake@output[[1]])
 
