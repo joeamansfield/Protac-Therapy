a29fd1bfbbdac81d04d69ce19db885494fe272f1
David Stone <stoned4@byu.edu>
2023-02-21 19:21:01 -0700

    Fixing files

--- Code/E3List.R
+++ Code/E3List.R
@@ -1,6 +1,13 @@
 library(data.table)
 
 data = fread(snakemake@input[[1]])
+uniqueData = data.frame(unique(data$enyz))
+colnames(uniqueData) = c('Uniprot')
 
-fwrite(list(unique(data$enyz)), snakemake@output[[1]])
+mapping = fread(snakemake@input[[2]], header=FALSE)
+colnames(mapping) = c('Uniprot', 'HGNC')
+
+joinedData = merge(x = uniqueData, y = mapping, by = 'Uniprot', all.x=TRUE)
+
+fwrite(list(joinedData$HGNC), snakemake@output[[1]])
 
--- Code/targetProteinList.R
+++ Code/targetProteinList.R
@@ -1,5 +1,14 @@
+## About 550 of these uniprot ids do not map to HGNC, so they have not been included unfortunately
+
 library(data.table)
 
 data = fread(snakemake@input[[1]])
+uniqueData = data.frame(unique(data$sub))
+colnames(uniqueData) = c('Uniprot')
 
-fwrite(list(unique(data$sub)), snakemake@output[[1]])
+mapping = fread(snakemake@input[[2]], header=FALSE)
+colnames(mapping) = c('Uniprot', 'HGNC')
+
+joinedData = merge(x = uniqueData, y = mapping, by = 'Uniprot', all.x=TRUE)
+
+fwrite(list(na.omit(joinedData$HGNC)), snakemake@output[[1]])
