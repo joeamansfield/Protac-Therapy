eaec75943727d3c3af95dd3e26d43b54c87bedb4
David Stone <stoned4@byu.edu>
2023-04-09 14:15:30 -0600

    Myabe fixed conda

--- Code/InitialSetup.sh
+++ Code/InitialSetup.sh
@@ -23,23 +23,22 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 conda create -y -n E3v1
 source activate E3v1
-conda install -c bioconda snakemake
+conda install -c bioconda snakemake -y
 conda install -c anaconda pandas -y
-conda install -c bioconda grid -y
-conda install -c conda-forge r-janitor -y
-conda deactivate
-
-conda create -y -n pymolv1
-conda activate pymolv1
-conda install -c schrodinger pymol-bundle -y
-conda deactivate
-
-conda create -y -n cptacv1
-source activate cptacv1
-conda install python=3.6 -y
 conda install -c bioconda cptac -y
+conda install -c conda-forge r-janitor -y
 conda install -c bioconda r-ggrepel -y
 conda install -c conda-forge r-viridis -y
 conda install -c conda-forge r-data.table -y
 conda install -c conda-forge r-tidyverse -y
+conda deactivate
+
+conda create -y -n pymolv1
+source activate pymolv1
+conda install -c schrodinger pymol-bundle -y
+conda deactivate
+
+conda create -y -n gridv1
+source activate gridv1
+conda install -c bioconda grid -y
 conda deactivate
